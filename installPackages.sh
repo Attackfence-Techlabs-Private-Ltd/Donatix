af615ec4d208cdce5cd9fd6bb01676c34c61c6e0
deepank <deepank@acpl.co.in>
2023-12-19 10:35:33 +0530

    File Update

---
+++ installPackages.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+#==========================================================================#
+# Set Script Variables                                                     #
+#==========================================================================#
+installerLog="/tmp/donaticsLinuxInstallation.log"
+GREEN='\033[0;92m'      #Green
+NOCOLOR='\033[0;37m'         #White
+ORANGE='\033[0;33'      #Orange
+CurrentDir=$(pwd)
+
+echo -e "=========================================" | tee -a ${installerLog}
+echo -e "      Installing AttackFence Sensor      " | tee -a ${installerLog}
+echo -e "=========================================" | tee -a ${installerLog}
+echo -e "AttackFence>> Logging post install output and errors to: ${installerLog}"
+
+# Clear log and timestamp the beginning
+cat /dev/null > ${installerLog}
+echo -e "=====================================================================" >> ${installerLog}
+echo -e "            Log Started: $(date)                                     " >> ${installerLog}
+echo -e "=====================================================================" >> ${installerLog}
+
+# Check if tshark is already installed
+if command -v tshark &> /dev/null; then
+    echo -e "AttackFence>> tshark is already installed. Skipping installation." | tee -a ${installerLog}
+else
+    # Install tshark
+    apt install tshark -y >> ${installerLog} 2>&1
+
+    if [ $? -ne 0 ]; then
+        echo -e "\r\e[0KAttackFence>> Sensor Dependencies Installation\t[${RED}FAILED${NOCOLOR}]" | tee -a ${installerLog}
+        exit 1
+    else
+        echo -e "\r\e[0KAttackFence>> Sensor Dependencies Installation\t[${GREEN}SUCCESS${NOCOLOR}]" | tee -a ${installerLog}
+    fi
+fi
+
+# Check if pip3 is already installed
+if command -v pip3 &> /dev/null; then
+    echo -e "AttackFence>> pip3 is already installed. Skipping installation." | tee -a ${installerLog}
+else
+    apt install python3-pip -y >> ${installerLog} 2>&1
+fi
+
+# Check if sqlite3 is already installed
+if command -v sqlite3 &> /dev/null; then
+    echo -e "AttackFence>> sqlite3 is already installed. Skipping installation." | tee -a ${installerLog}
+else
+    apt install sqlite3 -y >> ${installerLog} 2>&1
+fi
+# Check if expect is already installed
+if command -v expect &> /dev/null; then
+    echo -e "AttackFence>> expect is already installed. Skipping installation." | tee -a ${installerLog}
+else
+    apt install expect -y >> ${installerLog} 2>&1
+fi
+#==========================================================================#
+# Pre-installation checks                                                  #
+#==========================================================================#
+# Ensure the user exexuting this installer is root #
+if [ $(id --user) -ne 0 ]; then
+  echo "AttackFence>> Error: this script must be run as root. Exiting now ..." >> ${installerLog}
+  exit 1
+fi
+echo -e "AttackFence>> Installing Sensor Dependencies" | tee -a >> ${installerLog}
+
+while :;do for s in / - \\ \|; do printf "\r\e[0KAttackFence>> Installing Sensor Dependencies Please wait ....$s";sleep 1;done;done &
+
+if [ $? -ne 0 ]; then
+    echo -e "\r\e[0KAttackFence>> Sensor Dependencies Installation\t[${RED}FAILED${NOCOLOR}]" | tee -a ${installerLog}
+    exit 1
+else
+    echo -e "\r\e[0KAttackFence>> Sensor Dependencies Installation\t[${GREEN}SUCCESS${NOCOLOR}]" | tee -a ${installerLog}
+fi
+
+pip3 install aiohttp
+
+mkdir -p /opt/attackfence/NDR/tsharkQueryData/ >> ${installerLog} 2>&1
+chown -R attackfence:attackfence /opt/attackfence
+mv $CurrentDir/tsharkPackages/tsharkServices/* /etc/systemd/system/
+mv $CurrentDir/tsharkPackages/pythonScripts/* /opt/attackfence/NDR/tsharkQueryData/
+cd /etc/systemd/system/
+
+# enable services
+systemctl enable atf_tshark_query.service
+systemctl enable atf_dga_evaluation.service
+systemctl enable atf_dns_data_insertion.service
+systemctl enable atf_beaconing_hosts.service
+systemctl enable atf_dns_tunneling_hosts.service
+systemctl enable atf_ti_verdict.service
+
+# start services
+systemctl start atf_tshark_query.service
+systemctl start atf_dga_evaluation.service
+systemctl start atf_dns_data_insertion.service
+systemctl start atf_beaconing_hosts.service
+systemctl start atf_dns_tunneling_hosts.service
+systemctl start atf_ti_verdict.service
+
+kill $!; trap 'kill $!' SIGTERM
+retval=$?
+if [ $retval -ne 0 ]; then
+    echo -e "\r\e[0KAttackFence>> Sensor Configurations Updation\t[${RED}FAILED${NOCOLOR}]" | tee -a ${installerLog}
+    exit 1
+else
+    echo -e "\r\e[0KAttackFence>> Sensor Configurations Updation\t[${GREEN}SUCCESS${NOCOLOR}]" | tee -a ${installerLog}
+fi
+
+echo -e "AttackFence>> Attackfence Sensor Installed Successfully" | tee -a ${installerLog}
---
+++ tsharkPackages/pythonScripts/tsharkQuery.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+batch_size=1000  # Adjust the batch size as needed
+total_packets=10000  # Set the total number of packets you want to capture
+duration_per_batch=300  # Set the duration (in seconds) for each capture batch
+
+
+# Loop until the total number of packets is reached
+while [ $total_packets -gt 0 ]; do
+    # Capture a batch of packets
+    # tshark query to capture the dns records based on the batch size 
+    # and then the python script will insert the data in sqlite3 db
+    # unbuffer tshark -i w;p0s20f3 -c $batch_size -f "(udp src port 53) or (udp src port 5353) or (udp dst port 53) or (udp dst port 5353)" -n -T fields -e frame.interface_name -e _ws.col.Time -e ip.dst -e udp.dstport -e ip.src -e udp.srcport -e dns.flags.rcode -e dns.qry.name -e dns.qry.name.len -e dns.count.labels -e dns.qry.type -e dns.resp.name -e dns.resp.type -e dns.resp.ttl -e dns.resp.len -e dns.flags.response -e dns.resp.class -e dns.rr.udp_payload_size -e dns.a -e dns.aaaa -e dns.txt -e dns.ptr.domain_name -E header=y -E separator="," -E occurrence=f -E aggregator="|" -t ad | ./noname.py
+
+    # Alternatively, you can capture for a specific duration
+    # tshark query to capture the dns records based on the duration 
+    # and then the python script will insert the data in sqlite3 db
+    unbuffer tshark -i ens33 -a duration:$duration_per_batch -f "(udp src port 53) or (udp src port 5353) or (udp dst port 53) or (udp dst port 5353)" -n -T fields -e frame.interface_name -e _ws.col.Time -e ip.dst -e udp.dstport -e ip.src -e udp.srcport -e dns.flags.rcode -e dns.qry.name -e dns.qry.name.len -e dns.count.labels -e dns.qry.type -e dns.resp.name -e dns.resp.type -e dns.resp.ttl -e dns.resp.len -e dns.rr.udp_payload_size -e dns.a -e dns.aaaa -e dns.txt -e dns.ptr.domain_name -E header=y -E separator="," -E occurrence=f -E aggregator="|" -t ad | ./noname.py
+
+    # Decrement the total number of packets
+    total_packets=$((total_packets - batch_size))
+
+    # Sleep for a short duration before capturing the next batch
+    sleep 1
+done
+
